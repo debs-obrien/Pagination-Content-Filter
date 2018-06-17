a12250dffc72440c48da10f6fd50fcd967362dbe
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2018-06-17 18:49:05 +0200

    pagination working

--- js/index.js
+++ js/index.js
@@ -1,27 +1,25 @@
 "use strict";
 let list = document.getElementsByClassName('student-item');
 let newList = [];
-const max = 10;
-let page = 1; // will change depending on link clicked
-let pageNum = page -1;
-let maxNum = (pageNum * 10) + max;
-let startPage = pageNum * max;
+let max = 10;
 const totalPagination = Math.ceil(list.length / max);
+let startPage = 0;
 
 
 const HideStudents = (list) => {
     for (let i = 0; i < list.length; i++) {
         list[i].setAttribute("style", "display: none;");
     }
-
 };
-const showPage = (list, startPage, pageNum, maxNum) => {
+
+const showPage = (list, startPage) => {
     HideStudents(list);
+    let endPage = startPage + max;
     newList = [];
-    if(maxNum >= list.length){
-        maxNum = list.length
+    if(endPage >= list.length){
+        endPage = list.length
     }
-    for (let i = startPage; i < maxNum; i++) {
+    for (let i = startPage; i < endPage; i++) {
         newList.push(list[i]);
     }
     newList.forEach(element => {
@@ -38,31 +36,43 @@ const createPagination = (totalPagination) => {
     newDiv.appendChild(ul);
 
     let newPage;
-    for (let i = 1; i <= totalPagination; i++) {
+    for (let i = 0; i < totalPagination; i++) {
         const link = document.createElement("a");
         const li = document.createElement("li");
         ul.appendChild(li);
         li.appendChild(link);
         link.setAttribute('href', '#');
         link.setAttribute('class', 'page-number');
-        newPage = i;
+        newPage = i + 1;
         link.innerHTML = newPage;
         const currentPage = document.getElementsByClassName('page-number');
-        currentPage[i-1].addEventListener('click', changePage);
-
-
+        currentPage[0].setAttribute('class', 'page-number active');
+        currentPage[i].addEventListener('click', changePage);
     }
 
 
 };
 
+const calculatePage = (startPage, max) => {
+    startPage = startPage * max;
+    return startPage;
+};
+
+const activeClass = () => {
+    if(event.target.className !== 'page-number active'){
+        let notActive = document.getElementsByClassName('active');
+        notActive[0].setAttribute('class', 'page-number');
+        event.target.setAttribute('class', 'page-number active');
+    }
+};
+
+
 const changePage = () => {
-    page = event.target.innerHTML;
-    console.log(page);
-    HideStudents(list);
-    //showPage(list, startPage, 6, maxNum);
+    startPage = (event.target.innerHTML -1) * max;
+    showPage(list, startPage);
+    activeClass();
 };
 
 createPagination(totalPagination);
-
-showPage(list, startPage, pageNum, maxNum);
+calculatePage(startPage, max);
+showPage(list, startPage);
